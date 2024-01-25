614c5901a844ce5400c7b9ffba84e7847881e9c5
Natalie Lusk <144201569+nklusk@users.noreply.github.com>
2024-01-25 02:42:03 -0500

    completed + commented

--- finalWaveCode/osc_p5draw/sketch.js
+++ finalWaveCode/osc_p5draw/sketch.js
@@ -1,12 +1,15 @@
-// receives osc messages from chataigne websocket
-// micro sends osc messages to chataigne websocket
-// chataigne maps osc to this code 
-// this code 
-// p5js draws using the coordinates from OSC.
+// receives osc messages from chataigne websocket;
+// micro sends osc messages to chataigne websocket;
+// chataigne maps osc to this code; 
+// this code converts decimal to integer that corresponds to 
+// pilot notes in an array;
+// notes are sent to pilot thru chataigne
+
+// adapted from prof. duarte's osc to websocket example
 
 var notes = ["03C" , "03D" , "03E" , "03G" , "04A" , "04C"]; // array of note values
     // to assign wave height values to and then send to pilot
-var host = '127.0.0.1:8080'; // address of the websockets server
+var host = '127.0.0.1:8888'; // address of the websockets server
 var socket; // the websocket connection
 
 function setup() {
@@ -22,19 +25,20 @@ function openHandler() {
 
 function messageHandler(event) {
     var msg = event.data; // read data from the onmessage event
-    //waveHeights = msg; // puts osc message in wave number variable
     var note = notes[int(msg*6)] // converts received message to a note value
     console.log(note);
     playNote(note);
 }
 
 function playNote(note) {
+    var noteAsString = note.toString(); // converts note value to string 
+        // (not sure if this is necessary but it works this way)
 
     // send the note to the websocket server
     // (if the socket is open and ready)
     if (socket.readyState == 1) {
-        socket.send(note);
-        console.log("Sent: " + note);
+        socket.send(noteAsString); // sends note to websocket
+        console.log(noteAsString) 
     } else {
         console.log("Socket not ready.");
     }
